66e9fb8da0de56178fbb0a8732616c9c2d6e1ecf
blake kruppa <btruppa513@gmail.com>
2019-04-01 17:09:10 -0400

    added day 1 demos

---
+++ week1/02-variables.js
@@ -0,0 +1,9 @@
+// let and const were introduced in 2015 with the ES6 Standard of JS
+// and should be used in place of var.
+let a = 5; // use let or const
+const c = 25; // const can't be reasigned after it is created
+var v = 15; // don't user var use let or const
+
+console.log('a = ' + a);
+console.log('c = ' + c);
+console.log('v = ' + v + "\n\t don't use v");
