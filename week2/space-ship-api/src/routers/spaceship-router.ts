e26f3a43db54211cc76e70584ca2c8742f186e64
blake kruppa <btruppa513@gmail.com>
2019-04-12 13:58:23 -0400

    updated project to be closer to p0 requirements and added it to the week 2 foler as to not affect the week1 project as much

---
+++ week2/space-ship-api/src/routers/spaceship-router.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import * as spaceshipDao  from '../daos/spaceship.dao';
+import { authMiddleware } from '../middleware/auth.middleware';
+
+/**
+ * User router will handle all requests starting with
+ *  /users
+ */
+export const spaceshipRouter = express.Router();
+
+
+/**
+ * find all spaceships
+ * endpoint: /spaceships
+ */
+spaceshipRouter.get('', [
+  authMiddleware(['admin']),
+  async (req, res) => {
+  const ships = await spaceshipDao.findAllSpaceship();
+  res.json(ships);
+}]);
+
+/**
+ * find spaceship by id
+ * endpoint: /spaceships/:id
+ */
+spaceshipRouter.get('/:id', async (req, res) => {
+  const id = +req.params.id;
+  console.log(`retreiving spaceship with id: ${id}`);
+  res.json(await spaceshipDao.findById(id));
+});
+
+/**
+ * find spaceships by owner id
+ * endpoint: /spaceships/owner/:id
+ */
+spaceshipRouter.get('/owner/:ownerId', async (req, res) => {
+  res.json(await spaceshipDao.findByOwner(+req.params.ownerId));
+});
+
+spaceshipRouter.post('', (req, res) => {
+  console.log(`creating spaceship`, req.body);
+  res.status(201);
+  res.send('created spaceship');
+});
+
+spaceshipRouter.patch('', (req, res) => {
+  console.log(`updating spaceship`, req.body);
+  res.send('updated spaceship');
+});
