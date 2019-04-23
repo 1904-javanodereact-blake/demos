9197cbba9ea16a7f3efdaeda707642b616b0f975
blake kruppa <btruppa513@gmail.com>
2019-04-23 11:00:50 -0400

    added bootstrap and some other dependencies to the react application

--- week4/spaceship-app/src/App.tsx
+++ week4/spaceship-app/src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import './include/bootstrap';
 import './App.css';
+import { FirstComponent } from './components/first.component';
+import { SecondComponent } from './components/second.component';
+import { ThirdComponent } from './components/third.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BrowserRouter } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      My Application
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavComponent />
+        <FirstComponent />
+        My Application
+      <FirstComponent />
+        <FirstComponent />
+        hello
+      <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+      </div>
+    </BrowserRouter>
   );
 }
 
